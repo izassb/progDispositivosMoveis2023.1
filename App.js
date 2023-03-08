ebaf50d2e6d65cf6b8ec330e8930fd37e35495af
izassb <izabelly.00000830234@unicap.br>
2023-03-07 21:43:22 -0300

    start project

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Mundo João Rafael</Text>
+      <View style={styles.project}>
+      <Image
+          source={require('./assets/foloteBrinco.jpg')}
+          style={styles.projectImage}
+        />
+        <Text style={styles.projectDescription}>
+        Minha orelha
+        </Text>
+      </View>
+      <View style={styles.project}>
+      <Image
+          source={require('./assets/foloteEstranho.jpg')}
+          style={styles.projectImage}
+        />
+        <Text style={styles.projectDescription}>
+          sendo engraçado
+        </Text>
+      </View>
+      <View style={styles.project}>
+      <Image
+          source={require('./assets/folotePirata.jpeg')}
+          style={styles.projectImage}
+        />
+        <Text style={styles.projectDescription}>
+          vestido de pirata no carnaval 2019
+        </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +43,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  project: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    margin: 10,
+    borderRadius: 10,
+    width: '90%',
+  },
+  projectDescription: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  projectImage: {
+    width:250,
+    height:150,
+  }
 });
